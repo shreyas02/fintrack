7f40c76b399c1b7f483d0e7342b2f9a766549223
Shreyas <shreyasprashanth2002@gmail.com>
2021-03-26 21:50:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ search.addEventListener("input",function(){
     let noteCards = document.getElementsByClassName("noteCard");
     Array.from(noteCards).forEach(function(element){
         let cardTxt = element.getElementsByTagName("p")[0].innerText;
-        if(cardTxt.includes(inputVal)){
+        if(cardTxt.toLowerCase().includes(inputVal)){
             element.style.display = "block";
         }
         else{
